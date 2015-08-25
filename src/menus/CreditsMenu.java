feb0fc2b893c0a30ee3125084adb90af157e755e
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-08-25 00:08:03 -0600

    Commented

--- src/menus/CreditsMenu.java
+++ src/menus/CreditsMenu.java
@@ -24,14 +24,22 @@ public class CreditsMenu {
 	private JFrame creditsFrame;
 
 	public CreditsMenu() {
+		// Listener to activate once a button is pushed
 		ActionListener buttonListener = new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				switch (e.getActionCommand()) {
 				case EXIT_TEXT:
+					// Once the exit button is pushed
+
+					// Enable the main menu
 					MainMenu.menuFrame.setEnabled(true);
+
+					// Close the credits frame
 					creditsFrame.dispose();
+
+					// Stop the case
 					break;
 				}
 			}
@@ -39,44 +47,75 @@ public class CreditsMenu {
 
 		createCreditsMenu();
 
+		// Create a font for the menu to use
 		Font creditsMenuFont = new Font(Font.SANS_SERIF, Font.PLAIN, 50);
 
+		// Create a layout to use
 		GridBagLayout layout = new GridBagLayout();
 
+		// Set the frame's layout
 		creditsFrame.setLayout(layout);
 
+		// Set the padding to use for the items
 		Insets padding = new Insets(1, 1, 1, 1);
 
+		// Create a label for the credits
 		JLabel creditALabel = new JLabel(CREDIT_A);
+
+		// Set the font of the credit label
 		creditALabel.setFont(creditsMenuFont);
 
+		// Create a GridBagConstraints object for formatting
 		GridBagConstraints constraints = new GridBagConstraints();
+
+		// Set the fill
 		constraints.fill = GridBagConstraints.HORIZONTAL;
+
+		// Set width of each grid
 		constraints.gridwidth = 1;
-		constraints.gridx = 0;
-		constraints.gridy = 0;
+
+		// Set the padding
 		constraints.insets = padding;
 
+		// Plot at 0, 0
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+
+		// Add label
 		creditsFrame.add(creditALabel, constraints);
 
+		// Create a credit label
 		JLabel creditBLabel = new JLabel(CREDIT_B);
+
+		// Set the font of the credits
 		creditBLabel.setFont(creditsMenuFont);
 
+		// Plot at 0, 1
 		constraints.gridy = 1;
 
+		// Add label
 		creditsFrame.add(creditBLabel, constraints);
 
+		// Create button
 		JButton exitButton = new JButton(EXIT_TEXT);
+
+		// Set font to a modified version of the credits font
 		exitButton.setFont(creditsMenuFont.deriveFont(Font.PLAIN, 25));
+
+		// Add an action listener to the button
 		exitButton.addActionListener(buttonListener);
 
+		// Plot at 0, 2
 		constraints.gridy = 2;
 
+		// Add button
 		creditsFrame.add(exitButton, constraints);
 
+		// Add window listener
 		creditsFrame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
+				// Enable main menu
 				MainMenu.menuFrame.setEnabled(true);
 			}
 		});
@@ -84,6 +123,9 @@ public class CreditsMenu {
 		displayFrame();
 	}
 
+	/**
+	 * Sets up necessary aspects of the menu
+	 */
 	private void createCreditsMenu() {
 		// Create a new frame
 		creditsFrame = new JFrame();
@@ -98,6 +140,9 @@ public class CreditsMenu {
 		creditsFrame.getContentPane().setBackground(Color.WHITE);
 	}
 
+	/**
+	 * Display the menu
+	 */
 	private void displayFrame() {
 		// Size the frame
 		creditsFrame.pack();
